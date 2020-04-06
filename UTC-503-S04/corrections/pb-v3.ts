9d345a47b4f5e1637d570532d1ada2304e51963a
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-04-06 09:25:15 +0000

    Create pb-v3.ts

---
+++ UTC-503-S04/corrections/pb-v3.ts
@@ -0,0 +1,14 @@
+function defautSiNul(x: any, defaut: any): any {
+    if (x == null)
+        return defaut;
+    else
+        return x;
+}
+
+let maValeurParDefaut = 4;
+let maValeur = 1;
+let r: number = defautSiNul(maValeur, maValeurParDefaut);
+console.log(r);
+let maValeur2 = "Deux";
+r = defautSiNul(maValeur2, maValeurParDefaut);
+console.log(r);
