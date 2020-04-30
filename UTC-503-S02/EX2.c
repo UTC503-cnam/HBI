8b01a5897b0c7793412b7b2c83c660f0e19868ec
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-04-30 12:06:52 +0000

    Update EX2.c

--- UTC-503-S02/EX2.c
+++ UTC-503-S02/EX2.c
@@ -1,12 +1,18 @@
-void main () 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[]) 
 {
-int n = 5;
+    int n = 5;
 	int factorielle = n;
 	int i = n ;
+
     while (i > 1)
     {
         factorielle = factorielle * (i-1);
 		i = (i-1);    
+        
     }
-     printf("La factorielle de %d est égale à %d\n", n, factorielle);
+     printf ("La factorielle de %d est = %d\n", n, factorielle);
+    return EXIT_SUCCESS;
 }
