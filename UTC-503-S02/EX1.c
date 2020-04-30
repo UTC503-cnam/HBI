740b338d1c5b8bed2d540671248bcb4f9f6d62d3
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-04-30 13:06:45 +0000

    Update EX1.c

--- UTC-503-S02/EX1.c
+++ UTC-503-S02/EX1.c
@@ -9,8 +9,9 @@ int main (int argc, char* argv[])
 
     if (n != 0)
     {
-        do {
-            i = i - 1;
+        do 
+        {            
+            i--;
             factorielle = factorielle * i;
         }
         while (i > 1);
