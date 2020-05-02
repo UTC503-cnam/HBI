e1785ae36342447e795b3b9690fc83cbeeec5244
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-02 11:43:21 +0000

    Update EX2.c

--- UTC-503-S02/EX2.c
+++ UTC-503-S02/EX2.c
@@ -4,12 +4,12 @@
 int main (int argc, char* argv[]) 
 {
     int n = 5;
-	int factorielle = n;
-	int i = n ;
+    int factorielle = n;
+    int i = n ;
 
     for (i=n; i>1; i--)
     {
-        factorielle = factorielle * (i-1);
+        factorielle *= (i-1);
     }
      printf ("La factorielle de %d est = %d\n", n, factorielle);
     return EXIT_SUCCESS;
