6d936fc714d5df43272c03e11d99b5a775eed94a
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-05 21:45:04 +0000

    Update EX6.c

--- UTC-503-S02/EX6.c
+++ UTC-503-S02/EX6.c
@@ -1,20 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5 
 void mapDouble(int e[], int r[]){
+    int i;
+    for (i=0; i < N; i++)
+    {
+      r[i]= e[i] * 2;
+      printf ("%d ", r[i]);
+    }
 }
 
 void mapTriple(int e[], int r[]){
+  int i;
+    for (i=0; i < N; i++)
+    {
+      r[i]= e[i] * 3;
+      printf ("%d ", r[i]);
+    }
 }
 void mapSquare(int e[], int r[]){
+  int i;
+    for (i=0; i < N; i++)
+    {
+      r[i]= e[i] * e[i];
+      printf ("%d ", r[i]);
+    }
 }
 void mapMaximize3(int e[], int r[]){
 }
 
 int main() {
   int e[N] = {1, 5, 2, 4, 3};
-  int r_double[N]; mapDouble(e, r_double); 
+  int r_double[N]; 
+  mapDouble(e, r_double);
+  printf ("\n");
   int r_triple[N];
-  mapTriple(e, r_triple); 
+  mapTriple(e, r_triple);
+  printf ("\n"); 
   int r_square[N];
-  mapSquare(e, r_square); 
+  mapSquare(e, r_square);
+  printf ("\n"); 
   int r_maximize3[N];
   mapMaximize3(e, r_maximize3); 
 }
