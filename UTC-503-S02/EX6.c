a89fdac583359b0579e518cb9fee5f54066fcbdb
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-05 19:53:27 +0000

    Create EX6.c

---
+++ UTC-503-S02/EX6.c
@@ -0,0 +1,22 @@
+void mapDouble(int e[], int r[]){
+}
+
+void mapTriple(int e[], int r[]){
+}
+void mapSquare(int e[], int r[]){
+}
+void mapMaximize3(int e[], int r[]){
+}
+
+int main() {
+  int e[N] = {1, 5, 2, 4, 3};
+  int r_double[N]; mapDouble(e, r_double); 
+  int r_triple[N];
+  mapTriple(e, r_triple); 
+  int r_square[N];
+  mapSquare(e, r_square); 
+  int r_maximize3[N];
+  mapMaximize3(e, r_maximize3); 
+}
+
+  
