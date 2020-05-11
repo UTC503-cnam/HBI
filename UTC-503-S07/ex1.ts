686932745db3f8d964eb1d3cc3c6ce63c90a0791
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-11 13:14:58 +0000

    Create ex1.ts

---
+++ UTC-503-S07/ex1.ts
@@ -0,0 +1,5 @@
+// Ecrivez une version curryfiée de la fonction suivante
+// à l'aide de fonctions lambda 
+function format_message(from, to, message){
+    return "From: " + from + ", To: " + to + "Msg: " + message;
+}
