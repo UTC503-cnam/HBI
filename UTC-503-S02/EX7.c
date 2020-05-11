2bb08566a931c1f028a4e50d4e51dbe1fe1b8026
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-11 13:06:03 +0000

    Create EX7.c

---
+++ UTC-503-S02/EX7.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5
+void mapDouble(int e[], int r[])
+{
+    int i;
+    for (i=0; i < N; i++)
+    {
+      r[i] = e[i] * 2;
+      printf ("%d ", r[i]);
+    }
+}
+
+void mapTriple(int e[], int r[]){
+  int i;
+    for (i=0; i < N; i++)
+    {
+      r[i] = e[i] * 3;
+      printf ("%d ", r[i]);
+    }
+}
+void mapSquare(int e[], int r[]){
+  int i;
+    for (i=0; i < N; i++)
+    {
+      r[i] = e[i] * e[i];
+      printf ("%d ", r[i]);
+    }
+}
+void mapMaximize3(int e[], int r[])
+{
+  int i;
+    for (i=0; i < N; i++)
+    { if (e[i] <= 3)
+      {
+        r[i] = e[i];
+      }
+      else 
+      if (e[i] > 3)
+      {
+        r[i] = 3;
+      }
+      printf ("%d ", r[i]);
+    }
+}
+
+int my_double(int v){
+    return v * 2;
+}
+
+int my_triple(int v){
+    return v * 3;
+}
+  
+void map(int e[], int r[], int (*pointeurSurFonction)(int)){
+  for (int i = 0; i < N; i++)  
+    r[i] = pointeurSurFonction(e[i]);
+}
+
+int main() 
+{
+  int e[N] = {1, 5, 2, 4, 3};
+  int r_double[N]; 
+  mapDouble(e, r_double); 
+  printf ("\n");
+  int r_triple[N];
+  mapTriple(e, r_triple); 
+  printf ("\n");
+  int r_square[N];
+  mapSquare(e, r_square); 
+  printf ("\n");
+  int r_maximize3[N];
+  mapMaximize3(e, r_maximize3); 
+}
+
+  
