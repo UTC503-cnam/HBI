c230763411c0ab4ce088629a98430d81f2c510ed
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-05-11 13:07:01 +0000

    Create EX8.c

---
+++ UTC-503-S02/EX8.c
@@ -0,0 +1,3 @@
+Ce qui faudrait c'est le paradigme de programmation Objet,
+car on pourrait redefinir la méthode 'trace' avec un comportement spécifique pour les classes Triangle et Etoile.
+Ca s'appelle du polymorphisme.
