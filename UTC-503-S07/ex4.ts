e1a69af8f16198ab3810e9b06652f4784038b440
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 19:22:15 +0000

    Update ex4.ts

--- UTC-503-S07/ex4.ts
+++ UTC-503-S07/ex4.ts
@@ -1,5 +1,22 @@
 let numbers = [100, 34, 5, 67, 72];
-// En utilisant la récursion, écrivez le code permettant de trouver le min et le max de cette liste
-let minimum = numbers.// ...
-let maximum = numbers.// ...
+
+interface Array<T> {
+    min(): T;
+    max(): T;
+}
+
+Array.prototype.min = function<T>(){
+    let compute_min = arr => i => arr.length == 0?null:arr.length==1?arr[0]:i==arr.length - 1?arr[i]:Math.min(arr[i], compute_min(arr)(i + 1));
+    return compute_min(this)(0);
+}
+
+Array.prototype.max = function<T>(){
+    let compute_max = arr => i => arr.length == 0?null:arr.length==1?arr[0]:i==arr.length - 1?arr[i]:Math.max(arr[i], compute_max(arr)(i + 1));
+    return compute_max(this)(0);
+}
+
+
+let minimum = numbers.min();
+let maximum = numbers.max();
+
 console.log(`Min is ${minimum}, max is ${maximum}`);
