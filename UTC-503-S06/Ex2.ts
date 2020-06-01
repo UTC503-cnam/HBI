d46daf06cc699331660bc9f624185b22b257a2b2
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 16:50:11 +0000

    Update Ex2.ts

--- UTC-503-S06/Ex2.ts
+++ UTC-503-S06/Ex2.ts
@@ -1,7 +1,9 @@
-let tab = [1, 5, 2, 4, 3]
+let tab = [1, 5, 2, 4, 3];
 let r = tab.map(x => x * 2);
-console.log(tab); // [1, 5, 2, 4, 3]
-console.log(r); // [2, 10, 4, 8, 6]
-tab.map(x => x * x);
-tab.map(x => x > 3 ? 3 : x);
+let z = tab.map(x => x * 3);
+let t = tab.map(x => x * x);
 
+console.log(tab); 
+console.log(r);
+console.log(z);
+console.log(t);
