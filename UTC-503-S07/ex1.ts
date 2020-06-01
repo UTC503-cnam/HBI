a8c5a4970bdfbd8e2b29095a4895180941e6eab2
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 18:56:00 +0000

    Update ex1.ts

--- UTC-503-S07/ex1.ts
+++ UTC-503-S07/ex1.ts
@@ -1,1 +1,6 @@
-let format_message = from => to => message => "From: " + from + ", To: " + to + "Msg: " + message;
+let format_message = from => to => message => console.log ("From: " + from + ", To: " + to + " Msg: " + message);
+
+   
+let colonLogger = format_message("Alex");
+let my_logger = colonLogger("Bertrand");
+my_logger("Salut");
