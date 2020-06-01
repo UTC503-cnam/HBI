958cd62b6b1bb369151861ca2076fc0d9a699386
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 16:18:57 +0000

    Create Ex2.ts

---
+++ UTC-503-S06/Ex2.ts
@@ -0,0 +1,7 @@
+let tab = [1, 5, 2, 4, 3]
+let r = tab.map(x => x * 2);
+console.log(tab); // [1, 5, 2, 4, 3]
+console.log(r); // [2, 10, 4, 8, 6]
+tab.map(x => x * x);
+tab.map(x => x > 3 ? 3 : x);
+
