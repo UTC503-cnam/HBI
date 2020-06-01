eff6fb3ad52eb1f8912c615e58aaf3adced0e38c
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 17:25:32 +0000

    Delete map.ts

--- UTC-503-S07/map.ts
+++
@@ -1,3 +0,0 @@
-let authors = [{name: 'franquin'}, {name: 'uderzo'}, {name: 'hergé'}];
-let upperized_names = authors.map(n => n.name.toUpperCase());
-console.log(upperized_names); // [ 'FRANQUIN', 'UDERZO', 'HERGÉ' ]
