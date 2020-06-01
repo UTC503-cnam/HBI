be66c5d66d34d4c8ea6db423cfdbf172171fcee2
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 16:38:20 +0000

    Update Ex1.ts

--- UTC-503-S06/Ex1.ts
+++ UTC-503-S06/Ex1.ts
@@ -1,17 +1,7 @@
-function maximize_3(v){
-    if (v > 3)
-        return 3;
-    else
-        return v;
-}
-
-function maximize_4(v){
-    if (v > 4)
-        return 4;
-    else
-        return v;
-}
-
-let maximize_3_lambda = v => v > 3 ? 3 : v;
-let maximize_4_lambda = v => v > 4 ? 4 : v;
+ let maximize_3 = v => v > 3 ? 3 : v;
+ let maximize_4 = v => v > 4 ? 4 : v;
+ 
+ console.log (maximize_3(5));
+ console.log (maximize_4(2));
+ 
 
