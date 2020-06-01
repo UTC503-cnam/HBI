b82e68694366ebaffc674d29941a96f82ea2e61b
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-01 17:00:09 +0000

    Update Ex3.ts

--- UTC-503-S06/Ex3.ts
+++ UTC-503-S06/Ex3.ts
@@ -1,15 +1,7 @@
-function generateur_maximize(m){
-    return function(v){
-        return v > m ? m : v;
-    }
-}
-
-let generateur_maximize_lambda = m => v => v > m ? m : v;
+let generateur_maximize = m => v => v > m ? m : v;
 
 let maximize_3_os = generateur_maximize(3);
-let maximize_4_os = generateur_maximize_lambda(4);
+let maximize_4_os = generateur_maximize(4);
 
-tab.map(maximize_3_os);
-let res = maximize_3_os(10); 
-res = maximize_3_os(2); 
-generateur_maximize_lambda(4)(3);
+console.log(maximize_3_os(10));
+console.log(maximize_4_os(2));
