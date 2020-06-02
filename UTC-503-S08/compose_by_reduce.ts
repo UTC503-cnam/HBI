a1c3d24978684acea2af0e756bd0955995cb541c
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-02 22:29:46 +0000

    Update compose_by_reduce.ts

--- UTC-503-S08/compose_by_reduce.ts
+++ UTC-503-S08/compose_by_reduce.ts
@@ -1,10 +1,8 @@
-
 let square = x => x * x;
 let double = x => x * 2;
 let half = x => x / 2;
 let square_root = x => Math.sqrt(x);
 
-// Ecrivez la fonction lambda compose à l'aide de la fonction reduceRight
-let compose = 
+let compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 
 console.log('sqrt((10*10) * 2 / 2) = ' + compose(square_root, half, double, square)(10));
