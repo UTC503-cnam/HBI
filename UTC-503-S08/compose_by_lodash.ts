78773f1c2b268534702fc97bb841277983adb82a
benismailhamza <61600718+benismailhamza@users.noreply.github.com>
2020-06-02 22:29:13 +0000

    Update compose_by_lodash.ts

--- UTC-503-S08/compose_by_lodash.ts
+++ UTC-503-S08/compose_by_lodash.ts
@@ -1,6 +1,3 @@
-// A faire avant :
-// npm i --save lodash
-// npm i @types/node
 var fp = require('lodash/fp');
 
 let square = x => x * x;
@@ -8,5 +5,4 @@ let double = x => x * 2;
 let half = x => x / 2;
 let square_root = x => Math.sqrt(x);
 
-// Utilisez une fonction de lodash pour composer
-console.log('sqrt((10*10) * 2 / 2) = ' + ???(10));
+console.log('sqrt((10*10) * 2 / 2) = ' + fp.compose(square_root, half, double, square)(10));
